9b3dedbbad5aa6734ff21bf7c2035a87efa462b4
Saknet <andreas.v.andra@gmail.com>
2023-11-29 14:40:30 +0200

    updated vite.conf

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,26 @@
 import { defineConfig } from 'vite';
 import cesium from 'vite-plugin-cesium';
 import Vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from "url";
 
 export default defineConfig({
   plugins: [
     Vue(), 
     cesium()
   ],
-  server: {
+  server: { // for allowing any external access
     host: '0.0.0.0',
-    hmr: {
-        clientPort: 5173,
-        host: 'localhost',
-    },
     watch: {
-        usePolling: true
-    }
+      usePolling: true,
+    },
   },
-  base: './', // Ensure that paths resolve correctly within the Docker environment
+  resolve: {
+    alias: [
+      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+      { find: '@assets', replacement: fileURLToPath(new URL('./src/shared/assets', import.meta.url)) },
+      { find: '@cmp', replacement: fileURLToPath(new URL('./src/shared/cmp', import.meta.url)) },
+      { find: '@stores', replacement: fileURLToPath(new URL('./src/shared/stores', import.meta.url)) },
+      { find: '@use', replacement: fileURLToPath(new URL('./src/shared/use', import.meta.url)) },
+    ],
+  },
 });
