c8806e18129a81044378f1fd88d0f85014712039
Saknet <andreas.v.andra@gmail.com>
2023-11-29 11:58:19 +0200

    updated vite.conf

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,14 @@ export default defineConfig({
       usePolling: true,
     },
   },
+  resolve: {
+    alias: [
+        {
+            find: '@',
+            replacement: 'src',
+        },
+    ],
+    extensions: ['.vue', '.js']
+  },
   base: './', // Ensure that paths resolve correctly within the Docker environment
 });
