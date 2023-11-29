3cf718ffaf54c0af16f810495390057a53510df8
Saknet <andreas.v.andra@gmail.com>
2023-11-29 21:55:17 +0200

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite';
 import cesium from 'vite-plugin-cesium';
 import Vue from '@vitejs/plugin-vue';
 import path from 'path'
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 export default defineConfig({
   plugins: [
     Vue(), 
-    cesium()
+    cesium(),
+    pluginRewriteAll()
   ],
   server: { // for allowing any external access
     host: '0.0.0.0',
