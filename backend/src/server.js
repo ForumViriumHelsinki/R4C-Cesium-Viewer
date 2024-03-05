e3b7c86c3e24c5fc3e9aba37a4b3c30317be1d89
Saknet <andreas.v.andra@gmail.com>
2024-03-05 21:34:33 +0200

    added proxy for terrain

--- backend/src/server.js
+++ backend/src/server.js
@@ -51,5 +51,45 @@ app.get('/wms/proxy', async (req, res) => {
     }
 });
 
+app.get('/terrain-proxy/*', async (req, res) => {
+    let terrainUrl = 'https://kartta.hel.fi/3d/datasource-data/4383570b-33a3-4a9f-ae16-93373aff5ffa/' + req.params[0] + (req.url.includes('?') ? '&' : '?') + req.url.split('?')[1];
+        // Specific version substring to remove if present at the end of the URL
+        let versionSubstring = '&v=1.1708423032937557';
+
+        let und = '?undefined';
+
+        // Check if terrainUrl ends with the versionSubstring, and remove it if present
+        if (terrainUrl.endsWith(versionSubstring)) {
+            terrainUrl = terrainUrl.substring(0, terrainUrl.length - versionSubstring.length);
+        }
+
+        // Check if terrainUrl ends with the versionSubstring, and remove it if present
+        if (terrainUrl.endsWith(und)) {
+            terrainUrl = terrainUrl.substring(0, terrainUrl.length - und.length);
+        }
+    
+    console.log('Proxying request to:', terrainUrl);
+
+    try {
+        const response = await axios({
+            method: 'get',
+            url: terrainUrl,
+            responseType: 'arraybuffer',
+            httpsAgent: new https.Agent({  
+                rejectUnauthorized: false, // Ignore invalid SSL certificates if necessary
+            }),
+        });
+
+        // Set content type to the type of the terrain data
+        res.setHeader('Content-Type', response.headers['content-type']);
+        
+        // Send the response body back to the client
+        res.send(Buffer.from(response.data));
+    } catch (error) {
+        console.error('Error proxying terrain data:', error.message);
+        res.status(500).send('Error proxying terrain data');
+    }
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
