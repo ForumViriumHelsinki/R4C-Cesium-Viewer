442f51d216f7f68d938593272d6c244eb25d3619
Lauri Gates <lauri.gates@forumvirium.fi>
2024-10-29 20:07:20 +0200

    feat(r4c): Add test for HSY background maps
    Adds a test for the HSY background maps feature.
    
    The test checks that the background maps button is visible, that the search field is visible, and that the search results include the "Kaupunginosat pääkaupunkiseutu 2022" layer.

--- tests/r4c.spec.ts
+++ tests/r4c.spec.ts
@@ -1,7 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-test( 'test', async ( { page } ) => {
-  // await page.goto('https://geo.fvh.fi/r4c/M8Na2P0v6z/');
-  await page.goto( '/' );
-  await expect( page ).toHaveTitle( /R4C Uusimaa Demo/ );
+test( 'Page load', async ( { page } ) => {
+	await page.goto( '/' );
+	await expect( page ).toHaveTitle( /R4C Uusimaa Demo/ );
+} );
+
+test( 'HSY Background maps', async ( { page } ) => {
+	await page.goto( '/' );
+	await page.getByRole( 'button', { name: 'HSY Background maps' } ).click();
+	await page.getByPlaceholder( ' Search for WMS layers' ).click();
+	await page.getByPlaceholder( ' Search for WMS layers' ).fill( 'Kaupunginosat' );
+	await expect( page.locator( 'v-list-item-group' ) ).toContainText( 'Kaupunginosat pääkaupunkiseutu 2022' );
 } );
