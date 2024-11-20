4dffe6ac03252742bddd24ec3ab3bcaaef70dbc7
Lauri Gates <lauri.gates@forumvirium.fi>
2024-11-20 13:57:24 +0200

    chore: add sentry configuration (#54)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import * as Sentry from "@sentry/vue";
+import { createSentryPiniaPlugin } from "@sentry/vue";
 import App from './App.vue';
 
 // Vuetify
@@ -10,6 +12,12 @@ import * as directives from 'vuetify/directives';
 
 import { aliases, mdi } from 'vuetify/iconsets/mdi';
 import { md1 } from 'vuetify/blueprints';
+
+import { version } from '../package.json'
+
+// If you're using one of our framework SDK packages, like `@sentry/react`,
+// substitute its name for `@sentry/browser` here
+
 
 const vuetify = createVuetify( {
 	components,
@@ -25,7 +33,42 @@ const vuetify = createVuetify( {
 } );
 
 const pinia = createPinia();
+pinia.use(createSentryPiniaPlugin());
+
 const app = createApp( App );
+
+Sentry.init({
+	app,
+	// debug: true,
+  // Passing in `Vue` is optional, if you do not pass it `window.Vue` must be present.
+  // Vue: Vue,
+	dsn: import.meta.env.VITE_SENTRY_DSN,
+	environment: import.meta.env.MODE,
+	release: version,
+
+  // This enables automatic instrumentation (highly recommended),
+  // but is not necessary for purely manual usage
+  // If you only want to use custom instrumentation:
+  // * Remove the `BrowserTracing` integration
+  // * add `Sentry.addTracingExtensions()` above your Sentry.init() call
+  integrations: [
+  	Sentry.browserTracingIntegration(),
+  	Sentry.replayIntegration(),
+		Sentry.replayCanvasIntegration(),
+  ],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+
+  // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/r4c\.dataportal\.fi/],
+
+	// Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 app.use( pinia );
 app.use( vuetify );
