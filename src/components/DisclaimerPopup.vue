96e09c4eda13c35ecd07a43a65f2b2e2e5fc2d5d
Lauri Gates <lauri.gates@forumvirium.fi>
2024-11-22 13:27:40 +0200

    feat: update disclaimer (#59)

--- src/components/DisclaimerPopup.vue
+++ src/components/DisclaimerPopup.vue
@@ -6,10 +6,14 @@
   >
     <v-card>
       <v-card-title class="text-h5">
-        Disclaimer
+      Welcome to the R4C demonstration tool!
       </v-card-title>
       <v-card-text>
-        All data for development use only - Do not apply in planning.
+      The purpose of the demo tool is to visualize potential heat risk and socioeconomic vulnerability.
+      The demo tool helps to visualize the risks associated with climate change adaptation and the role of green infrastructure.
+      The formulas and calculations haven't undergone scientific peer review.
+      The visualizations offer a general indication of risks.
+      All data for development use only - Do not apply in planning.
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
