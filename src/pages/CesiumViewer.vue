2f913a166845ffbb2341632e91aeea57166c8f36
Saknet <andreas.v.andra@gmail.com>
2025-01-14 14:55:04 +0200

    fix #87 timeline is shown at building level

--- src/pages/CesiumViewer.vue
+++ src/pages/CesiumViewer.vue
@@ -9,7 +9,7 @@
 		</div>
 	    <!-- Loading Component -->
     	<Loading v-if="store.isLoading" />
-		<Timeline v-if="store.level === 'postalCode' && !toggleStore.helsinkiView "/>
+		<Timeline v-if="store.level !== 'start' && !toggleStore.helsinkiView "/>
     	<!-- Disclaimer Popup -->
     	<DisclaimerPopup class="disclaimer-popup" />
     	<BuildingInformation 
