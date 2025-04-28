7c383f852d4de1c2828b8f08695ed6661daf3487
Lauri Gates <lauri.gates@forumvirium.fi>
2025-04-28 12:33:36 +0300

    chore: update Sentry trace propagation targets
    Add r4c.dev.dataportal.fi to the list of trace propagation targets.

--- src/main.js
+++ src/main.js
@@ -62,7 +62,7 @@ Sentry.init({
   tracesSampleRate: 1.0,
 
   // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/r4c\.dataportal\.fi/],
+  tracePropagationTargets: ["localhost", /^https:\/\/r4c\.dataportal\.fi/, /^https:\/\/r4c\.dev\.dataportal\.fi/],
 
 	// Capture Replay for 10% of all sessions,
   // plus for 100% of sessions with an error
